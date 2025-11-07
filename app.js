a8e46836462b3941fda0fb719f9d3076d3bb05ee
clarex <233573238+clarexOps@users.noreply.github.com>
2025-11-07 14:25:00 -0800

    create variable to track count

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const count = document.getElementById("count");
+const countLabel = document.getElementById("count");
 const decreaseBtn = document.getElementById("decreaseBtn");
 const resetBtn = document.getElementById("resetBtn");
 const increaseBtn = document.getElementById("increaseBtn");
+
+let count = 0;
